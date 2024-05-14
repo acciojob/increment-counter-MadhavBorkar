1434c5da1accd9bb13c65f4bfb85940455d67ef9
MadhavBorkar <157888465+MadhavBorkar@users.noreply.github.com>
2024-05-14 22:49:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-//your JS code here. If required.
 document.addEventListener('DOMContentLoaded', function() {
     const counterElement = document.getElementById('counter');
     const incrementBtn = document.getElementById('incrementBtn');
- let counterValue = 0;
+    let counterValue = 0;
     counterElement.textContent = counterValue;
-  incrementBtn.addEventListener('click', function() {
-    alert(`Counter Value : ${counterValue}`);
-    counterValue++;
-    counterElement.textContent = counterValue;
-});
+    incrementBtn.addEventListener('click', function() {
+        counterValue++;
+        counterElement.textContent = counterValue;
+        alert(`Counter Value : ${counterValue}`);
+    });
 });
