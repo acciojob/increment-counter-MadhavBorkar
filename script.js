2773ee6a665fa6f7f03aaba3c3dae958a34aa501
MadhavBorkar <157888465+MadhavBorkar@users.noreply.github.com>
2024-05-18 00:25:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const counterElement = document.getElementById('counter');
     const incrementButton = document.getElementById('incrementBtn');
 
-    let counterValue = 0;
+    let counterValue = 3;
 
     incrementButton.addEventListener('click', () => {
         alert(`Current counter value: ${counterValue}`);
